1462ea320a49ea81af11bcfd62a7884f34c551f1
Rostyslav Matsko <rostyslav.matsko@student.tuke.sk>
2025-04-08 14:07:08 +0200

    Mine in first place fix

--- game.c
+++ game.c
@@ -2,33 +2,19 @@
 #include <assert.h>
 #include "game.h"
 
-
-/**
- * Change players score.
- * Adds value of opened tile to the score.
- * If game is solved/failed then multiples/divide the score by 2.
- */
 void update_player_score(Game *game, int input_row, int input_column) {
-
     if (game->game_state == FAILED) {
         game->player->score /= 2;
     } else {
-        game->player->score +=
-                game->board->tiles[input_row][input_column]->value;
+        game->player->score += game->board->tiles[input_row][input_column]->value;
     }
     if (game->game_state == SOLVED) {
         game->player->score *= 2;
     }
 }
 
-
-/**
- * Open neighbour Tile if value of the clue is 0.
- * Checks main and mixed directions of the opened Tile.
- */
 void open_neighbour_tiles(Game *game, int row, int column) {
     assert(game != NULL);
-    // iterates every possible direction of opened Tile
     for (int d_row = -1; d_row <= 1; d_row++) {
         for (int d_column = -1; d_column <= 1; d_column++) {
             int a_row = row + d_row;
@@ -41,44 +27,43 @@ void open_neighbour_tiles(Game *game, int row, int column) {
     }
 }
 
-
-/**
- * Create and allocate pointers of a Board, Player and Game.
- * Sets Game state to PLAYING.
- * @return pointer to the Game
- */
 Game *create_game() {
-    Game *game = (Game *) calloc(1, sizeof(Game));
-    Player *player = (Player *) calloc(1, sizeof(Player));
+    Game *game = (Game *)malloc(sizeof(Game));
+    Player *player = (Player *)malloc(sizeof(Player));
     game->player = player;
     game->player->score = 1;
     game->game_state = PLAYING;
     return game;
 }
 
+void move_mine(Board *board, int first_row, int first_column) {
+    int new_row = rand() % board->row_count;
+    int new_column = rand() % board->column_count;
+    while (board->tiles[new_row][new_column]->is_mine) {
+        new_row = rand() % board->row_count;
+        new_column = rand() % board->column_count;
+    }
+    board->tiles[new_row][new_column]->is_mine = 1;
+    board->tiles[first_row][first_column]->is_mine = 0;
+}
 
-/**
- * Open selected Tile(s) according to input coordinates.
- * Checks if input coordinates are in the correct range if Game state is playing.
- * Can change Game state to solved or failed.
- */
 void open_tile(Game *game, int input_row, int input_column) {
-
     if (game->game_state != PLAYING
         || !is_input_data_correct(game->board, input_row, input_column)) {
         return;
     }
 
     if (game->board->tiles[input_row][input_column]->tile_state == CLOSED) {
-        game->board->tiles[input_row][input_column]->tile_state = OPEN;
-
         if (game->board->tiles[input_row][input_column]->is_mine) {
             game->game_state = FAILED;
             open_all_mines(game->board);
+            game->player->score /= 2; // Halve player's score when game fails
+            return;
         }
 
-        if (!game->board->tiles[input_row][input_column]->is_mine
-            && game->board->tiles[input_row][input_column]->value == 0) {
+        game->board->tiles[input_row][input_column]->tile_state = OPEN;
+
+        if (game->board->tiles[input_row][input_column]->value == 0) {
             open_neighbour_tiles(game, input_row, input_column);
         }
         if (is_game_solved(game->board)) {
@@ -88,10 +73,6 @@ void open_tile(Game *game, int input_row, int input_column) {
     }
 }
 
-
-/**
- * Free pointers of the Player and Game.
- */
 void destroy_game(Game *game) {
     assert(game != NULL);
     destroy_board(game->board);
