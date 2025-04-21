f63c334662355bf3f4fe1f59207df996fab7430d
Archi816 <gaonly16@gmail.com>
2025-04-21 16:29:17 +0200

    fully working task3

--- main.c
+++ main.c
@@ -1,11 +1,39 @@
 #include <stdlib.h>
 #include "game.h"
+#include <stdio.h>
 #include "ui.h"
 #include "board.h"
 
+int get_valid_input(const char *prompt, int min, int max) {
+    int value;
+    while (1) {
+        printf("%s (%d - %d): ", prompt, min, max);
+        if (scanf("%d", &value) != 1 || value < min || value > max) {
+            printf("Neplatný vstup. Skúste znova.\n");
+            while (getchar() != '\n'); 
+        } else {
+            break;
+            printf("\n");
+        }
+    }
+
+
+
+    return value;
+}
+
+
+
+
+
 int main() {
+    int rows = get_valid_input("Zadajte počet riadkov", 1, 25);
+    int columns = get_valid_input("Zadajte počet stĺpcov", 1, 25);
+    int max_mines = rows * columns - 1;
+    int mines = get_valid_input("Zadajte počet mín", 1, max_mines);
+
     Game *game = create_game();
-    Board *board = create_board(9, 9, 9);
+    Board *board = create_board(rows, columns, mines);
     game->board = board;
     read_player_name(game);
     play_game(game);
